393e2c7dc60383399fc4bded5217f8be4d4fb240
Bendtner11 <bendtner.dave@gmail.com>
2023-02-15 16:19:09 +0000

    adding new doctype

---
+++ from_bank/loan_from_bank_management/doctype/external_loan_type/external_loan_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmed Fourati and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('External Loan Type', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ from_bank/loan_from_bank_management/doctype/external_loan_type/test_external_loan_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Ahmed Fourati and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestExternalLoanType(unittest.TestCase):
+	pass
